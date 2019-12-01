f6724f10af0087c3cd3d875b65b45001350afc0e
nehaganshani <54643730+nehaganshani@users.noreply.github.com>
2019-12-01 15:15:15 -0500

    created razor page

---
+++ SchoolHousing/SchoolHousing/Pages/SearchByZip.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AffordableHouseResponse;
+using ElementarySchoolResponse;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SchoolHousing.Pages
+{
+    public class SearchByZipModel : PageModel
+    {
+        private readonly ILogger<SearchByZipModel> _logger;
+        public SearchByZipModel(ILogger<SearchByZipModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+
+            SearchCompleted = false;
+
+        }
+        [BindProperty]
+        public string Search { get; set; }
+        public bool SearchCompleted { get; set; }
+        List<AffordableHouse> houseNearSchool = new List<AffordableHouse>();
+        ElementarySchool newSchool = new ElementarySchool();
+
+        public void OnPost()
+        {
+            string schoolJson = GetData("https://data.cityofchicago.org/resource/tj8h-mnuv.json");
+            ElementarySchool[] allSchool = ElementarySchool.FromJson(schoolJson);
+            string HouseJson = GetData("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
+            AffordableHouse[] allHouse = AffordableHouse.FromJson(HouseJson);
+            foreach (AffordableHouse house in allHouse)
+            {
+                if (house.ZipCode != null)
+                {
+                    
+                    
+                        houseNearSchool.Add(house);
+                    
+                }
+            }
+            foreach (ElementarySchool school in allSchool)
+            {
+                if (school.ZipCode != null)
+                {
+                    newSchool = school;
+                }
+            }
+            ViewData["Houses"] = houseNearSchool;
+            ViewData["School"] = newSchool;
+            SearchCompleted = true;
+        }
+        public string GetData(string endpoint)
+        {
+            string downloadedData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedData = webClient.DownloadString(endpoint);
+            }
+            return downloadedData;
+        }
+    }
+}
