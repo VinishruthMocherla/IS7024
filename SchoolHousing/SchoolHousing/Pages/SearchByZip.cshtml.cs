8a59dcd167ea4e26b6fbd204fb93e3d188e703af
nehaganshani <54643730+nehaganshani@users.noreply.github.com>
2019-12-01 20:04:08 -0500

    SearchByzip commit

--- SchoolHousing/SchoolHousing/Pages/SearchByZip.cshtml.cs
+++ SchoolHousing/SchoolHousing/Pages/SearchByZip.cshtml.cs
@@ -13,58 +13,38 @@ namespace SchoolHousing.Pages
 {
     public class SearchByZipModel : PageModel
     {
-        private readonly ILogger<SearchByZipModel> _logger;
-        public SearchByZipModel(ILogger<SearchByZipModel> logger)
-        {
-            _logger = logger;
-        }
+        public bool searchFinished { get; set; }
+        [BindProperty]
+        public long ZipSearch { get; set; }
+        public  AffordableHouseResponse.AffordableHouse[] houses;
+        public ElementarySchoolResponse.ElementarySchool[] schools;
+        public AffordableHouseResponse.AffordableHouse[] housesFiltered;
+        public ElementarySchoolResponse.ElementarySchool[] schoolsFiltered;
         public void OnGet()
         {
 
-            SearchCompleted = false;
-
         }
-        [BindProperty]
-        public string Search { get; set; }
-        public bool SearchCompleted { get; set; }
-        List<AffordableHouse> houseNearSchool = new List<AffordableHouse>();
-        ElementarySchool newSchool = new ElementarySchool();
+
 
         public void OnPost()
         {
-            string schoolJson = GetData("https://data.cityofchicago.org/resource/tj8h-mnuv.json");
-            ElementarySchool[] allSchool = ElementarySchool.FromJson(schoolJson);
-            string HouseJson = GetData("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
-            AffordableHouse[] allHouse = AffordableHouse.FromJson(HouseJson);
-            foreach (AffordableHouse house in allHouse)
+
+            using (var webClient = new WebClient())
             {
-                if (house.ZipCode != null)
-                {
-                    
-                    
-                        houseNearSchool.Add(house);
-                    
-                }
+                String schoolJSON = webClient.DownloadString("https://data.cityofchicago.org/resource/tj8h-mnuv.json");
+                schools = ElementarySchool.FromJson(schoolJSON);
+
+
+                String houseJSON = webClient.DownloadString("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
+                houses = AffordableHouse.FromJson(houseJSON);
+
+                housesFiltered = houses.Where(x => x.ZipCode == ZipSearch).ToArray();
+                schoolsFiltered = schools.Where(x => x.ZipCode == ZipSearch).ToArray();
+
+                ViewData["AffordableHouses"] = housesFiltered;
+                ViewData["ElementarySchools"] = schoolsFiltered;
             }
-            foreach (ElementarySchool school in allSchool)
-            {
-                if (school.ZipCode != null)
-                {
-                    newSchool = school;
-                }
-            }
-            ViewData["Houses"] = houseNearSchool;
-            ViewData["School"] = newSchool;
-            SearchCompleted = true;
-        }
-        public string GetData(string endpoint)
-        {
-            string downloadedData = "";
-            using (WebClient webClient = new WebClient())
-            {
-                downloadedData = webClient.DownloadString(endpoint);
-            }
-            return downloadedData;
+            searchFinished = true;
         }
     }
 }
