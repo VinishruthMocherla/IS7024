bd3228329a872551cf2cf0f0dbe39cd94624779f
Harsh Dave <harshdave1995@gmail.com>
2019-12-03 16:14:15 -0500

    Changes in several files
    Added img folder to render images locally.
    Removed redundant css properties.
    Removed redundant code on index page OnGet method.
    Enhanced rendering for mobile devices.

--- SchoolHousing/SchoolHousing/Pages/Index.cshtml.cs
+++ SchoolHousing/SchoolHousing/Pages/Index.cshtml.cs
@@ -10,16 +10,5 @@ using ElementarySchoolResponse;
 public class IndexModel : PageModel
 {
     public void OnGet()
-    {
-        using (var webClient = new WebClient())
-        {
-            string schoolString = webClient.DownloadString("https://data.cityofchicago.org/resource/tj8h-mnuv.json");
-            var schoolData = ElementarySchool.FromJson(schoolString);
-            ViewData["ElementarySchool"] = schoolData;
-
-            string houseString = webClient.DownloadString("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
-            var houseData = AffordableHouse.FromJson(houseString);
-            ViewData["AffordableHouse"] = houseData;
-        }
-    }
+    { }
 }
